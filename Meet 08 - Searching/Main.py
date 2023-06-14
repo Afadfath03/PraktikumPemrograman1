cc45dc3bcad129de2f3c5b015cb01418ebb34f74
Afadfath03 <Afadfath03@Gmail.com>
2023-06-14 09:33:50 +0700

    Completting Main

--- Meet 08 - Searching/Main.py
+++ Meet 08 - Searching/Main.py
@@ -10,34 +10,30 @@ def BubbleSort(Data):
     for i in range(len(Data)):
         for j in range(len(Data) - 1):
             if (str(Data[j]).lower() > str(Data[j + 1]).lower()):
-                temp = Data[j]
-                Data[j] = Data[j + 1]
-                Data[j + 1] = temp
+                Data[j], Data[j + 1] = Data[j + 1], Data[j]
     return Data
 
 def BinarySearch(Data, Keyword):
     BubbleSort(Data)
-    print(f'Array setelah diurutkan: {Data}')
-    
     low = 0
     high = len(Data) - 1
     while low <= high:
         mid = (low + high) // 2
-        
         if str(Data[mid]).lower() > Keyword.lower():
             high = mid - 1
         elif str(Data[mid]).lower() < Keyword.lower():
             low = mid + 1
         else:
-            print(f'{Keyword} ditemukan pada index ke-{mid}')
+            print(f'{Keyword} ditemukan pada index ke-{mid} setelah diurutkan')
             return mid
-        
+    
     print(f'{Keyword} tidak ditemukan')
     return -1
 
-arrayList = [8, 2, 6, 4, 7, 9, 1, 3, 5]
+arrayList = [23, 3, 4, 78, 9, 32]
 
 print(f'Array: {arrayList}')
-x = input("Masukan Data yang ingin dicari: ")
-print(LinearSearch(arrayList, x))
-print(BinarySearch(arrayList, x))
+x = input("Masukan Data yang ingin dicari:")
+print()
+# LinearSearch(arrayList, x)
+BinarySearch(arrayList, x)
