e71c3ce56a9e6b2a45ea58721eca4f26ae2a0db5
Afadfath03 <Afadfath03@Gmail.com>
2023-06-14 08:52:43 +0700

    Update Binary Search

--- Meet 08 - Searching/Main.py
+++ Meet 08 - Searching/Main.py
@@ -6,20 +6,33 @@ def LinearSearch(Data, Keyword):
     print(f'{Keyword} tidak ditemukan')
     return -1
 
-# def BinarySearch(arrayList, x):
-    left = 0
-    right = len(arrayList) - 1
-    mid = 0
+def BubbleSort(Data):
+    for i in range(len(Data)):
+        for j in range(len(Data) - 1):
+            if (str(Data[j]).lower() > str(Data[j + 1]).lower()):
+                temp = Data[j]
+                Data[j] = Data[j + 1]
+                Data[j + 1] = temp
+    return Data
 
-    while left <= right:
-        mid = (right + left) // 2
-
-        if arrayList[mid] < x:
-            left = mid + 1
-        elif arrayList[mid] > x:
-            right = mid - 1
+def BinarySearch(Data, Keyword):
+    BubbleSort(Data)
+    print(f'Array setelah diurutkan: {Data}')
+    
+    low = 0
+    high = len(Data) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if str(Data[mid]).lower() > Keyword.lower():
+            high = mid - 1
+        elif str(Data[mid]).lower() < Keyword.lower():
+            low = mid + 1
         else:
+            print(f'{Keyword} ditemukan pada index ke-{mid}')
             return mid
+        
+    print(f'{Keyword} tidak ditemukan')
     return -1
 
 arrayList = [8, 2, 6, 4, 7, 9, 1, 3, 5]
@@ -27,4 +40,4 @@ arrayList = [8, 2, 6, 4, 7, 9, 1, 3, 5]
 print(f'Array: {arrayList}')
 x = input("Masukan Data yang ingin dicari: ")
 print(LinearSearch(arrayList, x))
-
+print(BinarySearch(arrayList, x))
